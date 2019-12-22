66318be37fde67ef716c8c37f8d9e78fa1b3211c
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2019-12-22 10:55:53 -0500

    sync create_database

--- pysirix/sync/rest.py
+++ pysirix/sync/rest.py
@@ -7,11 +7,22 @@ def get_info(self, ret: bool):
         },
     )
     if response.status_code == 200:
-        self._instance_data.database_info += response.json()["databases"]
+        self._instance_data.database_info = response.json()["databases"]
     else:
         print(response)
     if ret:
         return self._instance_data.database_info
 
-def create_database():
-    pass
+
+def create_database(self, db_name, db_type):
+    response = self._session.get(
+        f"{self._instance_data.sirix_uri}/{db_name}",
+        headers={
+            "Authorization": f"Bearer {self._auth_data.access_token}",
+            "Accept": "application/json" if db_type == "json" else "application/xml",
+        },
+    )
+    if response.status_code == 200:
+        self._instance_data.database_info += response.json()["databases"]
+    else:
+        print(response)
