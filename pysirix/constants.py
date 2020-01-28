cd10f75093bf00421eecd83e39164ef979d1a6f3
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-01-28 16:43:28 -0500

    add docstring

--- pysirix/constants.py
+++ pysirix/constants.py
@@ -2,6 +2,9 @@ import enum
 
 
 class Insert(enum.Enum):
+    """
+    This Enum class defines the possible options for a resource update
+    """
     CHILD = "asFirstChild"
     LEFT = "asLeftSibling"
     RIGHT = "asRightSibling"
