5e4752e4e1134fa5da98184eedc7a40594aed6fa
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-03-22 14:00:09 -0400

    Update info.py

--- pysirix/info.py
+++ pysirix/info.py
@@ -1,19 +1,25 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Dict, List
 
 
-@dataclass
-class InstanceData:
-    sirix_uri: str
-    database_info: List[Dict[str, str]] = field(default_factory=lambda: [])
+DatabaseInfo = List[Dict[str, str]]
 
 
 @dataclass
 class AuthData:
     username: str
     password: str
-    keycloak_uri: str = None
     client_id: str = None
     client_secret: str = None
-    access_token: str = field(default_factory=lambda: "")
-    refresh_token: str = field(default_factory=lambda: "")
+
+
+@dataclass
+class TokenData:
+    access_token: str
+    expires_in: int
+    refresh_expires_in: int
+    refresh_token: str
+    token_type: str
+    not_before_policy: int
+    session_state: str
+    scope: str
