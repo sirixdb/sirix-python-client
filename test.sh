f3b29e027775cbe5d02bb277c3b36bb0f210ff62
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-03-23 15:53:08 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ docker-compose -f ./tests/resources/docker-compose.yml up -d keycloak
 ./test_resources/wait.sh
 sleep 15
 docker-compose -f ./tests/resources/docker-compose.yml up -d server
-sleep 5
+sleep 15
