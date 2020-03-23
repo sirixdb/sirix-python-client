580c2e6c4f37088325d52c6cfc28d75175d44afd
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-03-23 16:04:46 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo "starting the docker environment"
 docker-compose -f ./tests/resources/docker-compose.yml up -d keycloak
-./tests/resources/wait.sh
+bash ./tests/resources/wait.sh
 sleep 15
 docker-compose -f ./tests/resources/docker-compose.yml up -d server
 sleep 15
