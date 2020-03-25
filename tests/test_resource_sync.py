e7fe98c0429c8c9189c4c82176e75b619ce68eeb
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-03-25 10:03:05 -0400

    refactor some tests

---
+++ tests/test_resource_sync.py
@@ -0,0 +1,99 @@
+import httpx
+import pytest
+from httpx import HTTPError
+
+import pysirix
+from pysirix import DBType
+
+base_url = "https://localhost:9443"
+verify = "tests/resources/cert.pem"
+
+client = None
+sirix = None
+resource = None
+
+
+def setup_function():
+    global client
+    global sirix
+    global resource
+    client = httpx.Client(base_url=base_url, verify=verify)
+    sirix = pysirix.sirix_sync("admin", "admin", client)
+    db = sirix.database("First", DBType.JSON)
+    resource = db.resource("test_resource")
+
+
+def teardown_function():
+    sirix.delete_all()
+    client.close()
+
+
+def test_delete_resource():
+    resource.create([])
+    resource.delete(None, None)
+    assert resource.exists() is False
+
+
+def test_delete_nonexistent_resource():
+    assert resource.exists() is False
+    with pytest.raises(HTTPError):
+        resource.delete(None, None)
+
+
+def test_read_resource():
+    resource.create([])
+    data = resource.read(None)
+    assert data == []
+
+
+def test_get_etag():
+    resource.create([])
+    etag = resource.get_etag(1)
+    assert type(etag) == str
+
+
+def test_get_etag_nonexistent():
+    resource.create([])
+    with pytest.raises(HTTPError):
+        resource.get_etag(2)
+
+
+def test_delete_by_node_id():
+    resource.create([])
+    resource.delete(1, None)
+    with pytest.raises(HTTPError):
+        resource.delete(1, None)
+
+
+def test_delete_by_etag():
+    resource.create([])
+    etag = resource.get_etag(1)
+    resource.delete(1, etag)
+    with pytest.raises(HTTPError):
+        resource.delete(1, None)
+
+
+def test_update_by_etag():
+    resource.create([])
+    etag = resource.get_etag(1)
+    resource.update(1, {}, etag=etag)
+    assert resource.read(None) == [{}]
+
+
+def test_update_by_node_id():
+    resource.create([])
+    resource.update(1, {})
+    assert resource.read(None) == [{}]
+
+
+def test_update_nonexistent_node():
+    resource.create([])
+    with pytest.raises(HTTPError):
+        resource.update(5, {})
+
+
+def test_history():
+    resource.create([])
+    resource.update(1, {})
+    resource.delete(2, None)
+    assert len(resource.history()) == 3
