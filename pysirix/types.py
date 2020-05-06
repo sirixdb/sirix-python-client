825db026b2281816ad19b7ee3b2f2b35abbc2a17
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-05-06 08:04:22 -0400

    Update types.py

--- pysirix/types.py
+++ pysirix/types.py
@@ -3,17 +3,29 @@ try:
 
 
     class QueryResult(TypedDict):
+        """
+        This type is available only in python 3.8+.
+        Otherwise, defaults to ``dict``.
+        """
         revisionNumber: int
         revisionTimestamp: str
         revision: Dict
 
     class Commit(TypedDict):
+        """
+        This type is available only in python 3.8+.
+        Otherwise, defaults to ``dict``.
+        """
         revisionTimestamp: str
         revision: int
         author: str
         commitMessage: str
 
     class InsertDiff(TypedDict):
+        """
+        This type is available only in python 3.8+.
+        Otherwise, defaults to ``dict``.
+        """
         nodeKey: int
         insertPositionNodeKey: int
         insertPosition: str
@@ -21,11 +33,19 @@ try:
         depth: int
 
     class ReplaceDiff(TypedDict):
+        """
+        This type is available only in python 3.8+.
+        Otherwise, defaults to ``dict``.
+        """
         nodeKey: int
         type: str
         data: str
 
     class UpdateDiff(TypedDict):
+        """
+        This type is available only in python 3.8+.
+        Otherwise, defaults to ``dict``.
+        """
         nodeKey: int
         type: str
         value: Union[str, int, float, bool, None]
