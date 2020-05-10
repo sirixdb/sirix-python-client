983a7c8c7da46639040c2fd46c5e156e4da345f3
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2020-05-10 01:24:20 -0400

    add python 3.6 support

--- pysirix/info.py
+++ pysirix/info.py
@@ -1,29 +1,37 @@
-from dataclasses import dataclass
 from enum import Enum
 
+try:
+    from dataclasses import dataclass
 
-@dataclass
-class TokenData:
-    access_token: str
-    expires_in: int
-    expires_at: int
-    refresh_expires_in: int
-    refresh_token: str
-    token_type: str
-    not_before_policy: int
-    session_state: str
-    scope: str
+    @dataclass
+    class TokenData:
+        access_token: str
+        expires_in: int
+        expires_at: int
+        refresh_expires_in: int
+        refresh_token: str
+        token_type: str
+        not_before_policy: int
+        session_state: str
+        scope: str
+
+
+except ImportError:
+
+    class TokenData(dict):
+        def __getattr__(self, item):
+            return self.get(item)
 
 
 class NodeType(Enum):
-    OBJECT = "OBJECT",
-    ARRAY = "ARRAY",
-    OBJECT_KEY = "OBJECT_KEY",
-    OBJECT_STRING_VALUE = "OBJECT_STRING_VALUE",
-    STRING_VALUE = "STRING_VALUE",
-    OBJECT_NUMBER_VALUE = "OBJECT_NUMBER_VALUE",
-    NUMBER_VALUE = "NUMBER_VALUE",
-    OBJECT_BOOLEAN_VALUE = "OBJECT_BOOLEAN_VALUE",
-    BOOLEAN_VALUE = "BOOLEAN_VALUE",
-    OBJECT_NULL_VALUE = "OBJECT_NULL_VALUE",
+    OBJECT = ("OBJECT",)
+    ARRAY = ("ARRAY",)
+    OBJECT_KEY = ("OBJECT_KEY",)
+    OBJECT_STRING_VALUE = ("OBJECT_STRING_VALUE",)
+    STRING_VALUE = ("STRING_VALUE",)
+    OBJECT_NUMBER_VALUE = ("OBJECT_NUMBER_VALUE",)
+    NUMBER_VALUE = ("NUMBER_VALUE",)
+    OBJECT_BOOLEAN_VALUE = ("OBJECT_BOOLEAN_VALUE",)
+    BOOLEAN_VALUE = ("BOOLEAN_VALUE",)
+    OBJECT_NULL_VALUE = ("OBJECT_NULL_VALUE",)
     NULL_VALUE = "NULL_VALUE"
