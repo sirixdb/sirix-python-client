a1a9cfb720f82fdfa5778f5173df13c1ba4248b9
Moshe Uminer <47164590+mosheduminer@users.noreply.github.com>
2021-11-17 08:48:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysirix",
-    version="0.5.2",
+    version="0.5.3",
     author="Moshe Uminer",
     author_email="mosheduminer@gmail.com",
     description="The SirixDB Python SDK",
