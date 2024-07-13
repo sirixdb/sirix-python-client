97056f19675b6d977fd0a7be1857aab935e43188
Johannes Lichtenberger <Lichtenberger.Johannes@gmail.com>
2024-07-13 13:49:08 +0200

    Update wait.sh

--- tests/resources/wait.sh
+++ tests/resources/wait.sh
@@ -1,1 +1,1 @@
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://keycloak:8080/realms/master)" != "200" ]]; do sleep 5; done'
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/realms/master)" != "200" ]]; do sleep 5; done'
