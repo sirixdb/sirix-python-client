5fe12e714178609bd053f64858052122f840ad2f
Johannes Lichtenberger <Lichtenberger.Johannes@gmail.com>
2024-07-13 13:39:40 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo "starting the docker environment"
 docker-compose -f ./tests/resources/docker-compose.yml up -d keycloak
-bash ./tests/resources/wait-for-keycloak.sh
+bash ./tests/resources/wait.sh
 sleep 5
 docker-compose -f ./tests/resources/docker-compose.yml up -d server
 sleep 5
