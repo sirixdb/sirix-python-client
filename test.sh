188128d93c2919b7f4700124ef2f8c6d060179d8
johannesl <lichtenberger.johannes@gmail.com>
2024-07-14 12:08:45 +0200

    Add missing file

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo "starting the docker environment"
 docker-compose -f ./tests/resources/docker-compose.yml up -d keycloak
 # bash ./tests/resources/wait.sh
-sleep 30
+sleep 5
 docker-compose -f ./tests/resources/docker-compose.yml up -d server
-sleep 30
+sleep 5
